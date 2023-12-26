4612a91a720cb6c044cb2a5f3b75df65dae31cae
johnnyzhi1983 <59434240+johnnyzhi1983@users.noreply.github.com>
2023-12-26 15:45:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ import openai
 
 st.title("Recall Analysis")
 
+openai.api_key = st.secrets["openAI_SK"]
+
+
 def get_data_from_DB() -> pd.DataFrame:
   con = sqlite3.connect('recall.db')
   df = pd.read_sql_query("SELECT * FROM tb_recalls", con)
@@ -15,5 +18,3 @@ def get_data_from_DB() -> pd.DataFrame:
 
 df = get_data_from_DB()
 st.dataframe(df)
-
-#st.write(st.secrets["openAI_SK"])
